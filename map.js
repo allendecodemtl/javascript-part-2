93e1711c76ced9bddaee0cb12575ac058ad5095e
Allen <allen.decodemtl@gmail.com>
2017-01-11 23:07:52 +0000

    map -> commit #2

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 function Tile (type){
     this.type = type;  
-    this.walkable = function{
+    this.walkable = function(){
         if (this.type === "grass" || this.type === "sand"){
             return true
         } else {
@@ -12,5 +12,30 @@ function Tile (type){
 function Map(width, height){
     this.width = width;
     this.height = height;
-    
+    this.titles = twoD(width, height);
 }
+
+
+function twoD(width, height){
+    
+    var rows = [];
+    
+    for (var i =0; i<height; i++){
+        
+        var cols = [];
+        
+        var tileTypeList = ["water","grass","sand"];
+        var titleType = tileTypeList[Math.floor(Math.random()*tileTypeList.length)];
+         
+        for (var j=0; j<width; j++){
+            cols.push(new Tile(titleType));
+        }
+        rows.push(cols);
+        
+    }
+    
+    return rows;
+}
+
+
+console.log(new Map(5,3));
