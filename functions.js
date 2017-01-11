283ce6b6401353b623dc7bc193c0408713a154f3
Allen <allen.decodemtl@gmail.com>
2017-01-11 20:13:45 +0000

    functions -> commit #3

--- functions.js
+++ functions.js
@@ -22,7 +22,7 @@ var arr = [
     {name: 'joe', email:'joe@gmail.com'},
 ];
 
-// sort by longest namae
+// sort by longest name
 arr.sort(function(str1, str2) {
   return str1.name.length - str2.name.length;
 });
@@ -53,12 +53,35 @@ console.log(arr);
 // Then, use this function with map on an array of numbers to check the result.
 
 var numbers = [1, 4, 9];
-var roots = numbers.map(function(num){
+var sqr2 = numbers.map(function(num){
     return num * num;
 });
 
-console.log(roots);
+console.log(sqr2);
 
 // Create a function that can be used with Array.prototype.map. 
 // This function should be able to take an object and square its “num” property. 
 // Then, use this function with map on an array of objects each containming a “num” property.
+
+var arr3 = [
+    {num:2},    
+    {num:4},
+    {num:5},
+    {num:6}
+];
+
+var newArr3 = arr3.map(function(obj){ 
+   return obj.num * obj.num;
+});
+
+console.log(newArr3);
+
+
+// In a previous workshop, you had to create a function that took two numbers and an operation (add, sub, mult, …) 
+// and returned the result of the operation on the two numbers. 
+// Here we are going to do the same but at a higher order. 
+// Create a function called operationMaker that takes only a string called operation as argument. 
+// This string could be “add”, “subtract”, “mult” or “div”. 
+// Your function will return a function that will take two numbers and return the result of running operation on these numbers. 
+// The end result should let me do something like this:
+
